fe6cf2f2809eaa62d096ab174bf5de104eb2546c
Mike Parker <mikeparker1988@gmail.com>
2021-08-09 12:09:38 -0400

    first

---
+++ src/main/java/com/mike/cryptomonitor/CryptoMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.mike.cryptomonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mike/cryptomonitor/CryptoMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mike.cryptomonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
